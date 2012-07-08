c289e346b86f4b31a65ce75397aee850315d98d7
José Miguel Molina <rd4091@gmail.com>
2012-07-08 01:59:23 +0000

    Update master

--- oauth2.py
+++ oauth2.py
@@ -2,6 +2,7 @@
 The MIT License
 
 Copyright (c) 2007-2010 Leah Culver, Joe Stump, Mark Paschal, Vic Fryzel
+<https://github.com/simplegeo/python-oauth2>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
