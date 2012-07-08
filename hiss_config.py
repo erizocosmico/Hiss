193c6be4a85b30d21cf83abb0794fc1302caa0b4
José Miguel Molina Arboledas <rd4091@gmail.com>
2012-07-08 02:30:09 +0200

    Upload some files.

---
+++ hiss_config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+consumer_key = 'YOUR CONSUMER KEY'
+consumer_secret = 'YOUR CONSUMER SECRET KEY'
+use_dark_style = True
+
+def get_config():
+	return consumer_key, consumer_secret, style
